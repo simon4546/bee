5a817f670b6c49cf609bb66d324d0cdf073fb946
simon4546 <simon4545@126.com>
2021-04-10 16:15:48 +0800

    asdfasdf

--- shell/cash.sh
+++ shell/cash.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
-cat ./hostlist.txt | while read i
-do
-    USER=$1
-    IP=`echo $i | awk '{print $1}'`
-    echo "try $IP"
-    (ssh $USER@$IP "wget -O cashout.sh https://raw.githubusercontent.com/simon4546/bee/master/cashout.sh?token=ABVOP357GHRY5XKNWVRZJGTAOETUQ;chmod +x cashout.sh;bash ./cashout.sh cashout-all;" &)  2>> log.txt
-    #注意一定要在最后面加&符号，否则就是串行执行，不能体现并行。
-    #将错误重定向到日志文件中
- 
-    if [ $? -ne 0 ];then
-        echo "$IP执行过程中出现异常"
-    fi
-done
+function password() {
+    cat ./hostlist.txt | while read i
+    do
+        USER=$1
+        IP=`echo $i | awk '{print $1}'`
+        echo "try $IP"
+        (ssh $USER@$IP "cat /data/bee/password;cat /var/lib/bee/password;" & )  1>> passwd.txt
+        if [ $? -ne 0 ];then
+            echo "$IP执行过程中出现异常"
+        fi
+    done
+}
+function cashOut() {
+    cat ./hostlist.txt | while read i
+    do
+        USER=$1
+        IP=`echo $i | awk '{print $1}'`
+        echo "try $IP"
+        (ssh $USER@$IP "wget -O cashout.sh https://raw.githubusercontent.com/simon4546/bee/master/cashout.sh;chmod +x cashout.sh;bash ./cashout.sh cashout-all;" &)  2>> log.txt
+        #注意一定要在最后面加&符号，否则就是串行执行，不能体现并行。
+        #将错误重定向到日志文件中
+    
+        if [ $? -ne 0 ];then
+            echo "$IP执行过程中出现异常"
+        fi
+    done
+}
+
+case $1 in
+cashout)
+  cashOut $2
+  ;;
+password)
+  password $2
+  ;;
+esac
