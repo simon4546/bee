916288fa5a52bd28b57029ab21fc59cff2dff560
simon4546 <simon4545@126.com>
2021-04-12 12:59:38 +0800

    asdfasdf

--- shell/cash.sh
+++ shell/cash.sh
@@ -25,12 +25,30 @@ function cashOut() {
         fi
     done
 }
-
+function ulimitd() {
+    echo "正在执行ulimit"
+    cat ./hostlist.txt | while read i
+    do
+        USER=$1
+        IP=`echo $i | awk '{print $1}'`
+        echo "try $IP"
+        (ssh $USER@$IP "echo ulimit -n 50000 >>/etc/profile;source /etc/profile;ulimit -n" &) 2>&1>> ulimitd.txt
+        #注意一定要在最后面加&符号，否则就是串行执行，不能体现并行。
+        #将错误重定向到日志文件中
+    
+        if [ $? -ne 0 ];then
+            echo "$IP执行过程中出现异常"
+        fi
+    done
+}
 case $1 in
 cashout)
   cashOut $2
   ;;
 password)
   password $2
+  ;;
+ulimitd)
+  ulimitd $2
   ;;
 esac
